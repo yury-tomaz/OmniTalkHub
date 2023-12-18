67555f4de6127480d68190fc9125ff747e526619
Vila Dev <contato@viladev.com>
2023-12-18 03:47:34 -0300

    refactor(environment-variable): refactor environment configuration and add JWT key and database URL

--- packages/service-wa-bot/src/infrastructure/config/env.ts
+++ packages/service-wa-bot/src/infrastructure/config/env.ts
@@ -1,31 +1,25 @@
-const PORT = process.env.PORT || 3333;
-const WEBHOOK_ENABLED = (process.env.WEBHOOK_ENABLED && process.env.WEBHOOK_ENABLED === 'true') || false;
-const WEBHOOK_URL = process.env.WEBHOOK_URL || 'http://localhost:3333/webhook';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const WEBHOOK_BASE64 = (process.env.WEBHOOK_BASE64 && process.env.WEBHOOK_BASE64 === 'true') || false;
-const PROTECT_ROUTES = (process.env.PROTECT_ROUTES && process.env.PROTECT_ROUTES === 'true') || false;
-const MARK_MESSAGES_READ = (process.env.MARK_MESSAGES_READ && process.env.MARK_MESSAGES_READ === 'true') || false;
-const WEBHOOK_ALLOWED_EVENTS = process.env.WEBHOOK_ALLOWED_EVENTS?.split(',') || ['all'];
+const PORT = process.env.PORT || 3333;
+const JWT_KEY = process.env.JWT_KEY || 'secret';
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USER = process.env.DB_USER || 'user';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
-const DB_DATABASE = process.env.DB_DATABASE || 'database';
+const WEBHOOK_CONFIG = {
+  enabled: (process.env.WEBHOOK_ENABLED && process.env.WEBHOOK_ENABLED === 'true') || false,
+  url: process.env.WEBHOOK_URL || 'http://localhost:3333/webhook',
+  base64: (process.env.WEBHOOK_BASE64 && process.env.WEBHOOK_BASE64 === 'true') || false,
+  protectRoutes: (process.env.PROTECT_ROUTES && process.env.PROTECT_ROUTES === 'true') || false,
+  markMessagesRead: (process.env.MARK_MESSAGES_READ && process.env.MARK_MESSAGES_READ === 'true') || false,
+  allowedEvents: process.env.WEBHOOK_ALLOWED_EVENTS?.split(',') || ['all'],
+}
 
-export const env = {
+const  DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/wa-bot';
+
+export default {
   port: PORT,
-  webhookEnabled: WEBHOOK_ENABLED,
-  webhookUrl: WEBHOOK_URL,
-  webhookBase64: WEBHOOK_BASE64,
-  protectRoutes: PROTECT_ROUTES,
-  markMessagesRead: MARK_MESSAGES_READ,
-  webhookAllowedEvents: WEBHOOK_ALLOWED_EVENTS,
   logLevel: LOG_LEVEL,
-  db: {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-  }
+  webhook: WEBHOOK_CONFIG,
+  jwtKey: JWT_KEY,
+  database: DATABASE_URL
 }
