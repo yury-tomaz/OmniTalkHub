9c661267fe8dbcbc0a75337e8185002426c9e11f
Vila Dev <contato@viladev.com>
2023-12-18 03:44:58 -0300

    feat(prisma-middleware): add Prisma middleware for handling PrismaClient instances

--- packages/service-wa-bot/src/infrastructure/presentation/middlewares/prisma.middleware.ts
+++ packages/service-wa-bot/src/infrastructure/presentation/middlewares/prisma.middleware.ts
@@ -1,29 +1,31 @@
 import { PrismaClientManager } from "../../services/prisma-client-manager";
-import { NextFunction, Response } from "express";
-import { RequestWithTenant } from "./@types";
+import { NextFunction, Request, Response } from "express";
 import { AppError, HttpCode } from "../../../modules/@shared/domain/exceptions/app-error";
+import { PrismaClient } from "../../../generated/client";
 
 const prismaClientManager = PrismaClientManager.getInstance();
 
-export function prismaMiddleware(req: RequestWithTenant, res: Response, next: NextFunction) {
+export function prismaMiddleware(req: Request, res: Response, next: NextFunction) {
   const tenant =   req.headers['x-tenant-id'] as string;
 
   if (!tenant) {
     throw new AppError({
+      message: 'header x-tenant-id is missing',
       statusCode: HttpCode['BAD_REQUEST'],
-      message: 'Missing tenant id',
-      isOperational: true
+      isOperational: true,
     });
   }
 
-  const prismaClient = prismaClientManager.getClient(tenant);
+  const prismaClient: PrismaClient | null = prismaClientManager.getClient(tenant);
 
   if (!prismaClient) {
-    return res.status(500).send('Error creating PrismaClient');
+    throw new AppError({
+      message: `Error creating PrismaClient for tenant ${tenant}`,
+      statusCode: HttpCode['INTERNAL_SERVER_ERROR'],
+      isOperational: true,
+    });
   }
 
-  req['prisma'] = prismaClient as any;
-  req['tenant'] = tenant;
+  req['prisma'] = prismaClient as PrismaClient;
   next();
-
 }
