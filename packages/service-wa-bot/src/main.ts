f00e855f8bd62b0423ea8847f281eb2fad8e4b37
Vila Dev <contato@viladev.com>
2023-12-18 03:48:04 -0300

    fix: fix logger message typo

--- packages/service-wa-bot/src/main.ts
+++ packages/service-wa-bot/src/main.ts
@@ -1,7 +1,7 @@
-import { env } from "./infrastructure/config/env";
+import env from "./infrastructure/config/env";
 import { app } from "./infrastructure/express/app";
 import { logger } from "./infrastructure/logger";
 
 app.listen(env.port, () => {
- logger.info(`server run in por: ${env.port}`)
+ logger.info(`server run in por ${env.port}`)
 });
