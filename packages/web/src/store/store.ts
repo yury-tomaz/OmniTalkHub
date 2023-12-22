4d8e58b968e1ac8e4ad0ed8e2e9974b89421de0c
Vila Dev <contato@viladev.com>
2023-12-21 23:28:52 -0300

    feat(redux): impleent AuthReducer

--- packages/web/src/store/store.ts
+++ packages/web/src/store/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
 import CustomizerReducer from "./customizer/CustomizerSlice";
+import AuthReducer from "./auth/auth-slice";
 
 export const store = configureStore({
   reducer: {
     customizer: CustomizerReducer,
+    auth: AuthReducer,
   },
   devTools: process.env.NODE_ENV !== "production",
 });
 
 const rootReducer = combineReducers({
- customizer: CustomizerReducer,
+  customizer: CustomizerReducer,
+  auth: AuthReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
