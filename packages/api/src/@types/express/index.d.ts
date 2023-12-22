3d2cbee7183ed8882218be10f07e23d0a437120c
Vila Dev <contato@viladev.com>
2023-12-21 23:23:45 -0300

    feat(api): update Express Request interface
    - Added 'user' property to the 'Request' interface in the Express namespace.
    - Updated 'tenant' property to be inside the 'user' object.
    - Enhanced 'prisma' property to maintain compatibility.
    
    BREAKING CHANGE: 'tenant' property is now nested under 'user'.

--- packages/api/src/@types/express/index.d.ts
+++ packages/api/src/@types/express/index.d.ts
@@ -1,6 +1,25 @@
 declare namespace Express {
   export interface Request {
-   tenant: string | null;
-   prisma: PrismaClient | null;
+    user?: {
+      id: string,
+      email_verified: boolean,
+      name: string,
+      preferred_username: string,
+      given_name: string,
+      family_name: string,
+      email: string,
+      resource_access: {
+        account: {
+          roles: string[]
+        }
+      },
+      realm_access: {
+        roles: string[]
+      },
+      tenant: string;
+    };
+
+    prisma?: PrismaClient;
+
   }
 }
