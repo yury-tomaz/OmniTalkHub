3a72f2bd65c9ab6020149c6aa3a7f04f0db7ff3f
Vila Dev <contato@viladev.com>
2023-12-21 22:54:27 -0300

    chore: update import path for app module

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,5 +1,5 @@
 import env from "./infrastructure/config/env";
-import { app } from "./infrastructure/express/app";
+import { app } from "./infrastructure/presentation/app";
 import { logger } from "./infrastructure/logger";
 
 app.listen(env.port, () => {
