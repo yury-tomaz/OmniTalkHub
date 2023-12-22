fc64d36616db04cb554b702cd169d8668aa8f17a
Vila Dev <contato@viladev.com>
2023-12-21 23:30:00 -0300

    feat(auth): implement higher-order component for authentication
    - Created a higher-order component (HOC) 'WithAuth' for managing authentication.
    - Utilized Redux hooks to access authentication state and dispatch actions.
    - Used 'recoverTokens' function to attempt token recovery on component mount.
    - Handled login action dispatch and redirection on unsuccessful token recovery.
    
    BREAKING CHANGE: The structure of the authentication handling in the HOC has been updated.

---
+++ packages/web/src/HOCs/with-auth.tsx
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from "@/store/hooks";
+import { recoverTokens } from "@/utils/login";
+import { useEffect } from "react";
+import { login } from "@/store/auth/auth-slice";
+import { useRouter } from "next/navigation";
+
+interface PageParams {
+	params: {
+		id: string;
+	};
+}
+
+function WithAuth<P extends PageParams>(
+	WrappedComponent: React.ComponentType<P>
+) {
+	const WithAuthWrapper: React.FC<P> = (props: P) => {
+		const router = useRouter();
+		const auth = useSelector((state) => state.auth);
+		const dispatch = useDispatch();
+
+		useEffect(() => {
+			if(!auth.auth) {
+				try {
+					dispatch(login(recoverTokens()))
+				}catch (e) {
+					console.log(e);
+					router.push('/auth/login');
+				}
+			}
+		}, [auth]);
+
+		return <WrappedComponent {...props} />;
+	};
+
+	WithAuthWrapper.displayName = `withAuth(WithAuthWrapper)`;
+	return WithAuthWrapper;
+}
+
+export default WithAuth;
