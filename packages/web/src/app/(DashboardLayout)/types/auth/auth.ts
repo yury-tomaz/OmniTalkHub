7acf5b0e8797d173ada5339e1ef230d68ba3a405
Vila Dev <contato@viladev.com>
2023-12-21 23:36:28 -0300

    feat(types): add username, password, and submit functions to loginType interface

--- packages/web/src/app/(DashboardLayout)/types/auth/auth.ts
+++ packages/web/src/app/(DashboardLayout)/types/auth/auth.ts
@@ -8,6 +8,11 @@ export interface loginType {
   title?: string;
   subtitle?: JSX.Element | JSX.Element[];
   subtext?: JSX.Element | JSX.Element[];
+  username?: string;
+  password?: string;
+  setUserName: (value: string) => void;
+  setPassword: (value: string) => void;
+  submit: () => void;
 }
 
 export interface signInType {
