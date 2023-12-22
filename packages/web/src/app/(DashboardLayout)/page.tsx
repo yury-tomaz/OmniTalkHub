1b8ea1c73a23d7b9e3411c959fb9dfda14766e5d
Vila Dev <contato@viladev.com>
2023-12-21 23:37:31 -0300

    feat(auth): add WithAuth HOC

--- packages/web/src/app/(DashboardLayout)/page.tsx
+++ packages/web/src/app/(DashboardLayout)/page.tsx
@@ -2,17 +2,15 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
-// components
 import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
 import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
 import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
 import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
 import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
 import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings';
+import WithAuth from "@/HOCs/with-auth";
 
-
-export default function Dashboard (){
-
+function Dashboard (){
   return (
     <PageContainer title="Dashboard" description="this is Dashboard">
       <Box mt={3}>
@@ -46,3 +44,5 @@ export default function Dashboard (){
   )
 }
 
+
+export default WithAuth(Dashboard) as React.FC;
