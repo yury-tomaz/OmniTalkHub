bcd654bc8705ed19bc2194a0761679a073847545
Vila Dev <contato@viladev.com>
2023-12-27 01:49:07 -0300

    refactor: remove /health router

--- packages/api/src/presentation/routes/routes.ts
+++ packages/api/src/presentation/routes/routes.ts
@@ -1,11 +1,7 @@
-import {Router, Request, Response } from "express";
+import {Router} from "express";
 import v1 from "./v1/routes";
 const router = Router();
 
-router.get('/health', (req: Request, res: Response) => {
-  res.status(200);
-});
-
-router.use('/api/v1', v1);
+router.use('/api/v1', v1)
 
 export { router }
