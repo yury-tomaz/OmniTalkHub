7fa3060ee23ffdf44985c19a5e7ed01bccf45eb7
Vila Dev <contato@viladev.com>
2023-12-27 16:02:03 -0300

    feat(auth): add getClient method to AuthFacadeInterface
    #13

--- packages/api/src/infrastructure/providers/auth/facade/auth.facade.interface.ts
+++ packages/api/src/infrastructure/providers/auth/facade/auth.facade.interface.ts
@@ -1,7 +1,8 @@
 import { AuthClientServiceInputDTO, AuthClientServiceOutputDTO } from "../services/auth-code/auth-code.service";
-
+import { GetClientServiceInputDTO, GetClientServiceOutputDTO } from "../services/get-clients/get-clients.service";
 
 
 export interface AuthFacadeInterface {
  getAccessToken(input: AuthClientServiceInputDTO): Promise<AuthClientServiceOutputDTO>;
+ getClient(input:GetClientServiceInputDTO): Promise<GetClientServiceOutputDTO>;
 }
