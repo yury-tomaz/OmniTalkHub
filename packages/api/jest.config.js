e63b1d63db38c56d6c3a53531feaa4937fcdca09
Vila Dev <contato@viladev.com>
2023-12-27 01:52:41 -0300

    chore: adjust the configuration to use '@' path

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
