aacdb6daad8d1f55fbc0e2b87e8e09330e52e9fd
Vila Dev <contato@viladev.com>
2023-12-28 09:49:03 -0300

    feat(prisma): implement Prisma client manager per tenant
    - Adds a class to manage instances of PrismaClient based on the tenant
    - Provides methods to acquire, release, and create Prisma clients per tenant
    - Handles the creation of a new tenant based on a predefined template

--- packages/api/src/infrastructure/services/prisma-client-manager.ts
+++ packages/api/src/infrastructure/services/prisma-client-manager.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/client';
+import { PrismaClient } from "@prisma/client";
 import { logger } from "../logger";
 
 export class PrismaClientManager {
@@ -18,21 +18,23 @@ export class PrismaClientManager {
     return PrismaClientManager.instance;
   }
 
-  public getClient(tenant: string): PrismaClient | null {
+  public getClient(tenant: string): PrismaClient {
     try {
       if (!this.prismaClient[tenant]) {
         const databaseUrl = process.env.DATABASE_URL!.replace('public', tenant);
-
         this.prismaClient[tenant] = new PrismaClient({
+          log: ['query'],
           datasources: {
             db: {
               url: databaseUrl,
+              max: 10,
+              idleTimeoutMillis: 30000,
             },
           },
         });
+        logger.info(`PrismaClient for tenant ${tenant} created`);
       }
 
-      logger.info(`PrismaClient for tenant ${tenant} created`);
       return this.prismaClient[tenant];
     } catch (error: any) {
       logger.error(`Error creating PrismaClient for tenant ${tenant}: ${error.message}`);
@@ -47,6 +49,57 @@ export class PrismaClientManager {
       client.$disconnect();
       delete this.prismaClient[tenant];
       logger.info(`PrismaClient for tenant ${tenant} released`);
+    }
+  }
+
+  public async createNewTenant(tenant: string): Promise<void> {
+    try {
+      if (!this.prismaClient[tenant]) {
+        const databaseUrl = process.env.DATABASE_URL!.replace('public', 'tenant_template');
+        const prismaTenantTemplate = new PrismaClient({
+          log: ['query'],
+          datasources: {
+            db: {
+              url: databaseUrl,
+            },
+          },
+        });
+
+        const schemaTables = await prismaTenantTemplate.$queryRaw(`
+          SELECT table_name
+          FROM information_schema.tables
+          WHERE table_schema = 'tenant_template';
+        `);
+
+        await prismaTenantTemplate.$executeRaw`
+          CREATE SCHEMA ${tenant};
+        `;
+
+  
+        for (const table of schemaTables) {
+          await prismaTenantTemplate.$executeRaw(`
+            CREATE TABLE ${tenant}.${table.table_name} AS
+            SELECT * FROM tenant_template.${table.table_name};
+          `);
+        }
+
+        prismaTenantTemplate.$disconnect();
+        
+        this.prismaClient[tenant] = new PrismaClient({
+          log: ['query'],
+          datasources: {
+            db: {
+              url: process.env.DATABASE_URL!.replace('public', tenant),
+              max: 10,
+              idleTimeoutMillis: 30000,
+            },
+          },
+        });
+
+        logger.info(`PrismaClient for tenant ${tenant} created`);
+      }
+    } catch (error: any) {
+      logger.error(`Error creating PrismaClient for tenant ${tenant}: ${error.message}`);
     }
   }
 }
