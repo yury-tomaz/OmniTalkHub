39bd186fa6144562ced717c81208ac33eb5f4d38
Yury Tomaz <contato@viladev.com>
2023-12-31 22:41:02 -0300

    refactor: remove unused imports and event dispatching in AddWhatsappUseCase

--- packages/api/src/modules/whatsapp/application/comands/add-whatsapp/add-whatsapp.usecase.ts
+++ packages/api/src/modules/whatsapp/application/comands/add-whatsapp/add-whatsapp.usecase.ts
@@ -9,8 +9,6 @@ import EventDispatcherInterface from "@/modules/@shared/domain/events/event-disp
 export class AddWhatsappUseCase {
   constructor(
     private readonly whatsappService: WhatsappFactoryServiceInterface<Baileys>,
-    private readonly eventDispatcher: EventDispatcherInterface,
-    private readonly event: typeof WhatsappAddedEvent
   ) { }
 
   async execute(input: AddWhatsappDto) {
@@ -23,9 +21,7 @@ export class AddWhatsappUseCase {
     }
 
     const whatsapp = new Whatsapp(props);
-
     this.whatsappService.create(whatsapp);
-    this.eventDispatcher.notify(new this.event(props));
 
     return whatsapp;
   }
