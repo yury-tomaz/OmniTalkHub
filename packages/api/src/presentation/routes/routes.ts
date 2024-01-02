f3e13993fd8d5f75d97bbdb1f56891c1cb6cab89
Yury Tomaz <contato@viladev.com>
2024-01-02 08:42:40 -0300

    feat: add auth callback route and logger

--- packages/api/src/presentation/routes/routes.ts
+++ packages/api/src/presentation/routes/routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import v1 from "./v1/routes";
+import { logger } from "@/infrastructure/logger";
 const router = Router();
 
 router.use('/api/v1', v1);
+
+router.get('/auth/callback', (req, res) => {
+    logger.info('Auth callback');
+    res.send('OK')
+});
 
 export { router }
