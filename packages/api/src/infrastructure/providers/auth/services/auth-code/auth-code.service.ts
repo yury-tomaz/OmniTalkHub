59e19d4da7f5ecf88b2ebf9ef0243b1b4d905fac
Yury Tomaz <contato@viladev.com>
2024-01-02 08:43:17 -0300

    refactor: AuthCodeService execute method

--- packages/api/src/infrastructure/providers/auth/services/auth-code/auth-code.service.ts
+++ packages/api/src/infrastructure/providers/auth/services/auth-code/auth-code.service.ts
@@ -1,6 +1,5 @@
 import { AxiosInstance } from "axios";
 import { GetClientService } from "../get-clients/get-clients.service";
-import { AppError, HttpCode } from "@/modules/@shared/domain/exceptions/app-error";
 
 export interface AuthClientServiceInputDTO {
  realm: string
@@ -25,42 +24,32 @@ export class AuthCodeService {
   private readonly getClientService: GetClientService,
  ) { }
 
- public async execute(input: AuthClientServiceInputDTO): Promise<AuthClientServiceOutputDTO> {
-  const client = await this.getClientService.execute({
-   client_id: input.client_id,
-    realm: input.realm
-  });
-
-  if (!client) {
-   throw new AppError({
-    message: 'Client not found',
-    statusCode: HttpCode['BAD_REQUEST'],
-    isOperational: false
-   })
-  }
-
-  const endpoint = `/realms/${input.realm}/protocol/openid-connect/token`;
-
-  const formData = new URLSearchParams();
-
-  formData.append('grant_type', 'authorization_code');
-  formData.append('client_id', 'react_client');
-  formData.append('client_secret', client.secret);
-  formData.append('code', input.code);
-  formData.append('redirect_uri', 'http://localhost:3000/callback');
-
-  try{
-   const response = await this.apiKeycloak.post(endpoint, formData)
-   return response.data;
-  }catch(err: any){
-   throw new AppError({
-    message: err.response.data.error_description,
-    statusCode: err.response.status,
-    isOperational: false,
-    error: err.response.data.error
-   })
-  }
+  public async execute(input: AuthClientServiceInputDTO): Promise<AuthClientServiceOutputDTO> {
 
 
+    const endpoint = `/realms/${input.realm}/protocol/openid-connect/token`;
+    
+   
+      const client = await this.getClientService.execute({
+        client_id: input.client_id,
+        realm: input.realm
+      });
+        const response = await this.apiKeycloak.post(endpoint, {
+          grant_type: 'authorization_code',
+          client_id: input.client_id,
+          code: input.code,
+          redirect_uri: `http://localhost:3001/callback`,
+          client_secret: client.secret
+        },{
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          }
+      
+        })
+
+        return response.data;
+     
+   
+   
  }
 }
