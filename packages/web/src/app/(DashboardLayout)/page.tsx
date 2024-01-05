39ac5c54f6e93d3599df5345c0d25b0e3b1c9e0b
Yury Tomaz <contato@viladev.com>
2024-01-04 21:55:31 -0300

    refactor:  remove unused dashboard components

--- packages/web/src/app/(DashboardLayout)/page.tsx
+++ packages/web/src/app/(DashboardLayout)/page.tsx
@@ -5,9 +5,6 @@ import PageContainer from '@/app/(DashboardLayout)/components/container/PageCont
 
 import SalesOverview from '@/app/(DashboardLayout)/components/dashboard/SalesOverview';
 import YearlyBreakup from '@/app/(DashboardLayout)/components/dashboard/YearlyBreakup';
-import RecentTransactions from '@/app/(DashboardLayout)/components/dashboard/RecentTransactions';
-import ProductPerformance from '@/app/(DashboardLayout)/components/dashboard/ProductPerformance';
-import Blog from '@/app/(DashboardLayout)/components/dashboard/Blog';
 import MonthlyEarnings from '@/app/(DashboardLayout)/components/dashboard/MonthlyEarnings';
 import WithAuth from "@/HOCs/with-auth";
 
